e1ced8d159038c25f549a53533301d70d59b9d28
Кирилл Хмельницкий <the.kiritos@gmail.com>
2021-08-03 10:21:56 +0300

    refactor: increase speed in error handling

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,45 @@
-function checkValidSymbols(symbols: string): void | never {
+export const enum ValidationError {
+  MustBeString = 'Personal number must be string',
+  MustContain14symbols = 'Personal number must contain 14 symbols',
+  OnlyNumbersAndUppercaseLatin = 'Personal number must contain only numbers and uppercase Latin letters',
+  ControlNumbersAreNotEqual = 'Control numbers are not equal',
+}
+
+function checkValidSymbols(symbols: string) {
   if (typeof symbols !== 'string') {
-    throw new Error('Personal number must be string');
+    const error = {
+      tag: 'Err',
+      err: ValidationError.MustBeString,
+    } as const;
+    return error;
   }
   if (symbols.length !== 14) {
-    throw new Error('Personal number must contain 14 symbols');
+    const error = {
+      tag: 'Err',
+      err: ValidationError.MustContain14symbols,
+    } as const;
+    return error;
   }
   if (/^[0-9A-Z]*$/.test(symbols) === false) {
-    throw new Error(
-      'Personal number must contain only numbers and uppercase Latin letters'
-    );
+    const error = {
+      tag: 'Err',
+      err: ValidationError.OnlyNumbersAndUppercaseLatin,
+    } as const;
+    return error;
   }
+
+  const ok = { tag: 'Ok', ok: 'Symbols are valid' } as const;
+  return ok;
+}
+
+function getEncodedNumber(char: string, index: number): number {
+  const encodedNumber = Number.isInteger(+char)
+    ? +char
+    : char.charCodeAt(0) - 65 + 10;
+  return encodedNumber * [7, 3, 1][index % 3];
 }
 
 function getCalculatedControlNumber(personalNumber: string): number {
-  function getEncodedNumber(char: string, index: number): number {
-    const encodedNumber = Number.isInteger(+char)
-      ? +char
-      : char.charCodeAt(0) - 65 + 10;
-    return encodedNumber * [7, 3, 1][index % 3];
-  }
-
   return (
     Array.from(personalNumber.slice(0, -1)).reduce(
       (accum, curr, index) => accum + getEncodedNumber(curr, index),
@@ -28,30 +48,29 @@ function getCalculatedControlNumber(personalNumber: string): number {
   );
 }
 
-function compareControlNumbers(x: number, y: number): true | never {
-  if (x !== y) {
-    throw new Error('Control numbers are not equal');
-  }
-  return true;
+function compareControlNumbers(x: number, y: number) {
+  const ok = { tag: 'Ok', ok: 'Control numbers are equal' } as const;
+  const err = {
+    tag: 'Err',
+    err: ValidationError.ControlNumbersAreNotEqual,
+  } as const;
+  return x === y ? ok : err;
 }
 
 export function validate(
   personalNumber: string
-): { error: string; valid: false } | { error: null; valid: true } {
-  try {
-    checkValidSymbols(personalNumber);
-    const valid = compareControlNumbers(
-      getCalculatedControlNumber(personalNumber),
-      +personalNumber.slice(-1)
-    );
-    return {
-      error: null,
-      valid,
-    };
-  } catch (err) {
-    return {
-      error: err.message,
-      valid: false,
-    };
+): { valid: false; error: ValidationError } | { valid: true; error: null } {
+  const validateSymbolResult = checkValidSymbols(personalNumber);
+  if (validateSymbolResult.tag === 'Err') {
+    return { valid: false, error: validateSymbolResult.err };
   }
+
+  const compareResult = compareControlNumbers(
+    getCalculatedControlNumber(personalNumber),
+    +personalNumber.slice(-1)
+  );
+
+  return compareResult.tag === 'Ok'
+    ? { valid: true, error: null }
+    : { valid: false, error: compareResult.err };
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,47 +1,46 @@
 import test from 'ava';
-import { validate } from '../src/index';
+import { validate, ValidationError } from '../src/index';
 
 test('check valid personal number v1', t => {
   const result = validate('3270287A012PB2');
-  t.true(result.valid);
-  t.is(result.error, null);
+  t.deepEqual(result, { error: null, valid: true });
 });
 
 test('check valid personal number v2', t => {
   const result = validate('3041088A042PB2');
-  t.true(result.valid);
-  t.is(result.error, null);
+  t.deepEqual(result, { error: null, valid: true });
 });
 
 test('check valid personal number v3', t => {
   const result = validate('0000000A000PB2');
-  t.true(result.valid);
-  t.is(result.error, null);
+  t.deepEqual(result, { error: null, valid: true });
 });
 
 test('check invalid control number in personal number', t => {
   const result = validate('3270287A012PB3');
-  t.false(result.valid);
-  t.is(result.error, 'Control numbers are not equal');
+  t.deepEqual(result, {
+    error: ValidationError.ControlNumbersAreNotEqual,
+    valid: false,
+  });
 });
 
 test('check invalid type in personal number', t => {
   const result = validate(123 as any);
-  t.false(result.valid);
-  t.is(result.error, 'Personal number must be string');
+  t.deepEqual(result, { error: ValidationError.MustBeString, valid: false });
 });
 
 test('check invalid length in personal number', t => {
   const result = validate('3270287A012PB31');
-  t.false(result.valid);
-  t.is(result.error, 'Personal number must contain 14 symbols');
+  t.deepEqual(result, {
+    error: ValidationError.MustContain14symbols,
+    valid: false,
+  });
 });
 
 test('check invalid symbols in personal number', t => {
   const result = validate('3270287А012РВ3');
-  t.false(result.valid);
-  t.is(
-    result.error,
-    'Personal number must contain only numbers and uppercase Latin letters'
-  );
+  t.deepEqual(result, {
+    error: ValidationError.OnlyNumbersAndUppercaseLatin,
+    valid: false,
+  });
 });
